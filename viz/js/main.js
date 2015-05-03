ec1371018fb5e6e742a3ed3423aaf41a58362af3
Shyamal Ruparel <shyamal@ruparel.co>
2015-05-03 09:59:40 -0400

    Added Reset Controls.

--- viz/js/main.js
+++ viz/js/main.js
@@ -95,7 +95,7 @@ function genre(ndx){
     .group(genreSumGroup)
     .minAngleForLabel(.15)
     .innerRadius(60)
-    .ordinalColors(['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5','#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107','#ff9800', '#ff5722'])
+    .ordinalColors([ '#e91e63', '#9c27b0', '#673ab7', '#3f51b5','#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107','#ff9800', '#ff5722','#f44336'])
     .legend(dc.legend())
     .label(function(d) {
       percent = (Math.floor(d.value/genreDimension.groupAll().value() * 100));
@@ -116,7 +116,7 @@ function type(ndx){
     .dimension(typeDimension)
     .group(typeSumGroup)
     .innerRadius(60)
-    .ordinalColors([ '#ffeb3b', '#ffc107','#ff9800', '#ff5722','#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5','#2196f3','#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39'])    .minAngleForLabel(.15)
+    .ordinalColors([  '#ffc107','#ff9800', '#ff5722','#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5','#2196f3','#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39','#ffeb3b'])    .minAngleForLabel(.15)
     .legend(dc.legend())
     .label(function(d) {
       percent = (Math.floor(d.value/typeDimension.groupAll().value() * 100));
